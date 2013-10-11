5108172d10c26fce51cd4b46d99de497ebc45061
Grant Heffernan <heffergm@gmail.com>
2013-10-11 12:23:50 -0400

    canvas

--- attributes/apache.rb
+++ attributes/apache.rb
@@ -8,5 +8,5 @@ default[:tilestache][:apache][:max]               = 20
 default[:tilestache][:apache][:ttl]               = 300
 default[:tilestache][:apache][:retrytimeout]      = 3
 default[:tilestache][:apache][:connectiontimeout] = 3
-default[:tilestache][:apache][:base_uri]          = '/vector'
+default[:tilestache][:apache][:base_uri]          = '/vector/'
 default[:tilestache][:apache][:canvas_map]        = false
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'grant@mapzen.com'
 license          'All rights reserved'
 description      'Installs/Configures tilestache'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.9.14'
+version          '0.9.15'
 
 recipe 'tilestache', 'Installs tilestache'
 
