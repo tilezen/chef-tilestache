7de05b216461c700a11cbddbf4be8bd51bff8696
Grant Heffernan <heffergm@gmail.com>
2013-10-12 07:39:28 -0400

    expiresdefault

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'grant@mapzen.com'
 license          'All rights reserved'
 description      'Installs/Configures tilestache'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.9.15'
+version          '0.9.16'
 
 recipe 'tilestache', 'Installs tilestache'
 
