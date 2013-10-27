6d75520e6e1409df743fcfd87d29bd8880ec905d
Grant Heffernan <heffergm@gmail.com>
2013-10-27 08:30:25 -0400

    supervisor log

--- recipes/supervisor.rb
+++ recipes/supervisor.rb
@@ -16,8 +16,8 @@ supervisor_service 'tilestache' do
   stopwaitsecs 3
   stopsignal 'TERM'
   user "#{node[:tilestache][:user]}"
-  stdout_logfile "/var/log/supervisor.log"
-  stderr_logfile "/var/log/supervisor.log"
+  stdout_logfile "#{node[:tilestache][:gunicorn][:logdir]}/supervisor.log"
+  stderr_logfile "#{node[:tilestache][:gunicorn][:logdir]}/supervisor.log"
   directory "#{node[:tilestache][:cfg_path]}"
   command "/usr/local/bin/gunicorn \"TileStache:WSGITileServer('#{node[:tilestache][:cfg_path]}/#{node[:tilestache][:cfg_file]}')\" --log-file #{node[:tilestache][:gunicorn][:logdir]}/#{node[:tilestache][:gunicorn][:logfile]} -c #{node[:tilestache][:gunicorn][:cfgbasedir]}/#{node[:tilestache][:gunicorn][:cfg_file]}"
 end
