ee4082f88d4377cac55c387338aa730f78a5bfa2
Grant Heffernan <heffergm@gmail.com>
2013-11-12 06:17:51 -0500

    if run as root, dont create user

--- recipes/groundwork.rb
+++ recipes/groundwork.rb
@@ -7,14 +7,18 @@
 # All rights reserved - Do Not Redistribute
 #
 
-user_account "#{node[:tilestache][:user]}" do
-  manage_home true
-  home '/home/tilestache'
-  shell "#{node[:tilestache][:user_shell]}"
-  ssh_keygen node[:tilestache][:user_keygen]
-  create_group true
-  uid node[:tilestache][:uid]
-  gid node[:tilestache][:gid]
+# is someone tells us to run as root,
+#   don't muck with the account
+unless node[:tilestache][:user] == 'root'
+  user_account "#{node[:tilestache][:user]}" do
+    manage_home true
+    home '/home/tilestache'
+    shell "#{node[:tilestache][:user_shell]}"
+    ssh_keygen node[:tilestache][:user_keygen]
+    create_group true
+    uid node[:tilestache][:uid]
+    gid node[:tilestache][:gid]
+  end
 end
 
 user_ulimit "#{node[:tilestache][:user]}" do
