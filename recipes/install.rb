e7a69235e0a17f39639ebdff90d09dc8bcdac018
Grant Heffernan <heffergm@gmail.com>
2014-01-16 09:21:22 -0500

    clean

--- recipes/install.rb
+++ recipes/install.rb
@@ -38,6 +38,7 @@ end
 include_recipe 'tilestache::gunicorn'
 
 # if supervisor, then don't install init service
+#
 case node[:tilestache][:init_type]
 when 'supervisor'
   include_recipe 'tilestache::supervisor'
@@ -56,24 +57,6 @@ tilestacherc 'tilestache-config' do
     notifies :restart, 'service[tilestache]', :delayed
   end
 end
-
-# NOTE: there is an issue with the following if you run chef under 
-#   ruby1.8.7, in that it doesn't support ordered hashes. Since the key
-#   retrieval is random, the config file will be regenerated on every 
-#   chef run. Please upgrade to ruby1.9.x (I'm looking at you opsworks!)
-#file "#{node[:tilestache][:cfg_path]}/#{node[:tilestache][:cfg_file]}" do
-#  action :create
-#  owner 'root'
-#  group "#{node[:tilestache][:group]}"
-#  mode 0640
-#  content JSON.pretty_generate(node[:tilestache][:config_file_hash])
-#  case node[:tilestache][:supervisor]
-#  when true
-#    notifies :restart, 'supervisor_service[tilestache]', :delayed
-#  else
-#    notifies :restart, 'service[tilestache]', :delayed
-#  end
-#end
 
 include_recipe 'tilestache::apache'
 
