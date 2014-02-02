e81d9cf0ed95c681ed0c98552861600c36213782
Grant Heffernan <heffergm@gmail.com>
2014-02-02 07:28:40 -0500

    remove max-aget

--- attributes/apache.rb
+++ attributes/apache.rb
@@ -11,4 +11,3 @@ default[:tilestache][:apache][:retrytimeout]      = 3
 default[:tilestache][:apache][:connectiontimeout] = 3
 default[:tilestache][:apache][:requesttimeout]    = 60
 default[:tilestache][:apache][:base_uri]          = '/vector/'
-default[:tilestache][:apache][:max_age]           = 43200
