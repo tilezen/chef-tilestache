08180cb271c2c6832338fb68c8e093adf3f68a5d
Grant Heffernan <heffergm@gmail.com>
2014-03-03 08:11:33 -0500

    specs

--- spec/apache_spec.rb
+++ spec/apache_spec.rb
@@ -3,21 +3,21 @@ require 'spec_helper'
 describe 'tilestache::apache' do
   context 'include proxy' do
     let(:chef_run) do
-      ChefSpec::Runner.new(platform: 'ubuntu', version:  '12.04') do |node|
+      ChefSpec::Runner.new(platform: 'ubuntu', version:  '12.04', log_level: :debug) do |node|
         node.set[:tilestache][:apache_proxy] = true
       end.converge(described_recipe)
     end
 
-    # NOTE: need to define custom matchers
-    #
-    #it 'should include recipe apache2::default' do
-    #  chef_run.should include_recipe'apache2::default'
-    #end
+    it 'should include recipe apache2::default' do
+      chef_run.should include_recipe 'apache2::default'
+    end
 
-    #it 'should include disable the default apache sites' do
-    #  chef_run.should disable_apache_site 'default'
-    #  chef_run.should disable_apache_site '000-default'
-    #end
+    %w(proxy expires proxy_http proxy_connect).each do |mod|
+      it "should execute a2enmod #{mod}" do
+        chef_run.should run_execute "a2enmod #{mod}"
+      end
+    end
+
   end
 end
 
