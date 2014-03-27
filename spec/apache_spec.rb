58942bf16fa15f68bdef46060489711d1a8e6706
Grant Heffernan <heffergm@gmail.com>
2014-03-27 19:00:13 -0400

    specs

--- spec/apache_spec.rb
+++ spec/apache_spec.rb
@@ -17,7 +17,19 @@ describe 'tilestache::apache' do
         chef_run.should run_execute "a2enmod #{mod}"
       end
     end
-
   end
+
+  context 'senza proxy' do
+    let(:chef_run) do
+      ChefSpec::Runner.new do |node|
+        node.set[:tilestache][:apache_proxy] = false
+      end.converge(described_recipe)
+    end
+
+    it 'should not include recipe apache2::default' do
+      chef_run.should_not include_recipe 'apache2::default'
+    end
+  end
+
 end
 
