16c8c88a7aa61cfa72626bd8a891697eba274c90
Grant Heffernan <heffergm@gmail.com>
2014-03-28 13:40:54 -0400

    bump version for inclusion of python-redis

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'grant@mapzen.com'
 license          'All rights reserved'
 description      'Installs/Configures tilestache'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.10.2'
+version          '0.10.3'
 
 recipe 'tilestache', 'Installs tilestache'
 
