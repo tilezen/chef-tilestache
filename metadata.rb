d84c7ffe2c76decd0d8bb6c095a33f8c1b696dd6
Grant Heffernan <heffergm@gmail.com>
2014-03-29 13:54:45 -0400

    bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'grant@mapzen.com'
 license          'All rights reserved'
 description      'Installs/Configures tilestache'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.10.3'
+version          '0.11.0'
 
 recipe 'tilestache', 'Installs tilestache'
 
