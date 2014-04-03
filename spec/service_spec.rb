f8c80f068f883585952c22de7bfaf91854e56e97
Grant Heffernan <heffergm@gmail.com>
2014-04-03 15:02:39 -0400

    specs

--- spec/service_spec.rb
+++ spec/service_spec.rb
@@ -4,11 +4,18 @@ describe 'tilestache::service' do
   let(:chef_run) { ChefSpec::Runner.new.converge(described_recipe) }
 
   it 'should create file from template /etc/init.d/tilestache' do
-    chef_run.should create_template('/etc/init.d/tilestache').with(owner: 'root', group: 'root', mode: '0755', source: 'tilestache-init.erb')
+    chef_run.should create_template('/etc/init.d/tilestache').with(
+      owner:  'root',
+      group:  'root',
+      mode:   '0755',
+      source: 'tilestache-init.erb'
+    )
   end
 
   it 'should enable the tilestache service' do
-    chef_run.should enable_service('tilestache')
+    chef_run.should enable_service('tilestache').with(
+      supports: { start: true, stop: true, restart: true, status: true }
+    )
   end
 
 end
