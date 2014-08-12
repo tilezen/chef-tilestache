c6ab4962ca2df8b36b39e7900570396934b736de
Grant Heffernan <heffergm@gmail.com>
2014-08-12 10:58:08 -0400

    lint

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,25 +3,25 @@
 # Attributes:: default
 #
 
-default[:tilestache][:config][:include_sample]   = false
-default[:tilestache][:config][:mode]             = 0644
+default[:tilestache][:config][:include_sample] = false
+default[:tilestache][:config][:mode]           = 0644
 
-default[:tilestache][:init_type]          = 'runit'
-default[:tilestache][:version]            = '1.49.11'
-default[:tilestache][:filehandle_limit]   = 20_480
-default[:tilestache][:cfg_path]           = '/etc/tilestache'
-default[:tilestache][:cfg_file]           = 'tilestache.conf'
-default[:tilestache][:user]               = 'tilestache'
-default[:tilestache][:group]              = 'tilestache'
-default[:tilestache][:user_shell]         = '/bin/false'
-default[:tilestache][:user_keygen]        = false
-default[:tilestache][:apache_proxy]       = true
-default[:tilestache][:uid]                = nil
-default[:tilestache][:gid]                = nil
-default[:tilestache][:install_method]     = 'pip'
-default[:tilestache][:git_repository]     = 'https://github.com/mapzen/TileStache.git'
-default[:tilestache][:git_revision]       = 'master'
-default[:tilestache][:source_install_dir] = '/opt/tilestache_source'
+default[:tilestache][:init_type]               = 'runit'
+default[:tilestache][:version]                 = '1.49.11'
+default[:tilestache][:filehandle_limit]        = 20_480
+default[:tilestache][:cfg_path]                = '/etc/tilestache'
+default[:tilestache][:cfg_file]                = 'tilestache.conf'
+default[:tilestache][:user]                    = 'tilestache'
+default[:tilestache][:group]                   = 'tilestache'
+default[:tilestache][:user_shell]              = '/bin/false'
+default[:tilestache][:user_keygen]             = false
+default[:tilestache][:apache_proxy]            = true
+default[:tilestache][:uid]                     = nil
+default[:tilestache][:gid]                     = nil
+default[:tilestache][:install_method]          = 'pip'
+default[:tilestache][:git_repository]          = 'https://github.com/mapzen/TileStache.git'
+default[:tilestache][:git_revision]            = 'master'
+default[:tilestache][:source_install_dir]      = '/opt/tilestache_source'
 
 # python
-default[:python][:install_method] = 'package'
+default[:python][:install_method]              = 'package'
