7b90ae97cbfacb5657a8c3dadcb9a345cbbbb345
Robert Marianski <rob@marianski.com>
2014-12-03 14:00:11 -0500

    add mapbox-vector-tile package

--- attributes/pip_requirements.rb
+++ attributes/pip_requirements.rb
@@ -3,11 +3,12 @@ default[:tilestache][:pip_requirements_location]      = "#{Chef::Config[:file_ca
 # default python package versions when using pip_requirements install method
 default[:tilestache][:pip_requirements] = %w(
   argparse==1.2.1
-  python-memcached==1.53
+  mapbox-vector-tile==0.0.1
   ModestMaps==1.4.6
   Pillow==2.6.1
   protobuf==2.6.0
   psycopg2==2.5.4
+  python-memcached==1.53
   Shapely==1.4.3
   simplejson==3.6.4
   git+https://github.com/mapzen/TileStache@integration-1#egg=TileStache
