a27106ad14c7af50199183fde489fbfd55dd4abc
Robert Marianski <rob@marianski.com>
2015-01-28 14:53:02 -0500

    Remove no longer used apache_proxy variable

--- attributes/default.rb
+++ attributes/default.rb
@@ -15,7 +15,6 @@ default[:tilestache][:user]                    = 'tilestache'
 default[:tilestache][:group]                   = 'tilestache'
 default[:tilestache][:user_shell]              = '/bin/false'
 default[:tilestache][:user_keygen]             = false
-default[:tilestache][:apache_proxy]            = true
 default[:tilestache][:gunicorn_server]         = true
 default[:tilestache][:uid]                     = nil
 default[:tilestache][:gid]                     = nil
